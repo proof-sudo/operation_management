cf1cf05e31d1640da2f87da9248f254d0726c671
prince-rebel <djakaridjatraore1997@gmail.com>
2025-10-31 15:11:59 +0000

    Prise en compte des bons d'achat

--- odoo_sync_from_odoo11/models/inherit_purchase.py
+++ odoo_sync_from_odoo11/models/inherit_purchase.py
@@ -17,12 +17,12 @@ class PurchaseOrder(models.Model):
     ref_Fp = fields.Char(string='Commande Client / FP')
     client_id = fields.Many2one(string='Client', comodel_name='res.partner', ondelete='restrict')
     
-class DossierCommercial(models.Model):
-    _inherit = 'dossier.commercial'
+# class DossierCommercial(models.Model):
+#     _inherit = 'dossier.commercial'
     
-    purchase_order_ids = fields.One2many('purchase.order', 'dossier_id', string='Commandes d\'Achat Associées')
-    client_id = fields.Many2one(string='Client', comodel_name='res.partner', ondelete='restrict')
-    ref_bon_commande_client = fields.Char(string='Référence Bon de Commande Client')
+#     purchase_order_ids = fields.One2many('purchase.order', 'dossier_id', string='Commandes d\'Achat Associées')
+#     client_id = fields.Many2one(string='Client', comodel_name='res.partner', ondelete='restrict')
+#     ref_bon_commande_client = fields.Char(string='Référence Bon de Commande Client')
     
 
   
